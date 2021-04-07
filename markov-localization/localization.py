050c8dd32996790150b9f333d163df517d786c22
Sayan Goswami <goswami.sayan47@gmail.com>
2021-04-07 17:28:59 +0200

    Add localization.py.

---
+++ markov-localization/localization.py
@@ -0,0 +1,20 @@
+import numpy as np
+from environment import State, Stage, Direction, Environment
+
+
+if __name__ == "__main__":
+    map = [
+        [1, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+        [1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 1, 1, 0, 1, 1, 0, 1],
+        [1, 0, 0, 1, 0, 0, 1, 0, 0, 0],
+        [1, 1, 1, 1, 0, 0, 1, 0, 1, 0],
+        [1, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+        [0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+    ]
+
+    env = Environment(map)
+    belief = np.zeros((env.get_n_rows(), env.get_n_cols()))
