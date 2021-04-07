0b29dc812dc765b1e8bc418a68e02d56ef9fe7d7
Sayan Goswami <goswami.sayan47@gmail.com>
2021-04-07 17:48:26 +0200

    Calculate initial belief.

--- markov-localization/localization.py
+++ markov-localization/localization.py
@@ -17,4 +17,21 @@ if __name__ == "__main__":
     ]
 
     env = Environment(map)
+    free_cells = env.get_free_cells()
+
     belief = np.zeros((env.get_n_rows(), env.get_n_cols()))
+    for cell in free_cells:
+        r, c = cell
+        belief[r, c] = 1/len(free_cells)
+
+    print(belief)
+
+    actions = [
+        [Stage.PERCEPTION],
+        [Stage.PREDICTION, Direction.L],
+        [Stage.PERCEPTION],
+        [Stage.PREDICTION, Direction.U],
+        [Stage.PERCEPTION],
+        [Stage.PREDICTION, Direction.R],
+        [Stage.PERCEPTION]
+    ]
